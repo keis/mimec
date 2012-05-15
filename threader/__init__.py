9bbe2cd999d6331fe59204f72e42b5c781b3a810
David Keijser <david.keijser@klarna.com>
2012-05-15 20:34:45 +0200

    pep8ify threader

--- threader/__init__.py
+++ threader/__init__.py
@@ -4,105 +4,107 @@ import functools
 
 
 message = functools.partial(
-	collections.namedtuple('Message', ('id', 'subject', 'ref')),
-	subject=None,
-	ref=[]
+    collections.namedtuple('Message', ('id', 'subject', 'ref')),
+    subject=None,
+    ref=[]
 )
 
+
 class Container:
-	def __init__(self, message):
-		self.message = message
-		self._parent = None
-		self._children = set()
+    def __init__(self, message):
+        self.message = message
+        self._parent = None
+        self._children = set()
 
-	@property
-	def is_root(self):
-		return self._parent is None
+    @property
+    def is_root(self):
+        return self._parent is None
 
-	@property
-	def is_placeholder(self):
-		return self.message.subject is None
+    @property
+    def is_placeholder(self):
+        return self.message.subject is None
 
-	def can_reach(self, other):
-		assert self is not other
-		if other in self._children:
-			return True
-		return any(c.can_reach(other) for c in self._children)
+    def can_reach(self, other):
+        assert self is not other
+        if other in self._children:
+            return True
+        return any(c.can_reach(other) for c in self._children)
 
-	def prune(self):
-		newchildren = set()
-		for c in self._children:
-			newchildren.update(c.prune())
-		self._children = newchildren
+    def prune(self):
+        newchildren = set()
+        for c in self._children:
+            assert c._parent is self, '%r not parent of %r' % (self, c)
+            newchildren.update(c.prune())
+        self._children = newchildren
 
-		if self.is_placeholder:
-			if self._parent is None and len(self._children) != 1:
-				return [self]
+        if self.is_placeholder:
+            if self._parent is None and len(self._children) != 1:
+                return [self]
 
-			return newchildren
+            return newchildren
 
-		return [self]
-		
-	def add_child(self, other):
-		if self.can_reach(other) or other.can_reach(self):
-			#print('would loop %r => %r' % (self, other))
-			return
-		if other._parent:
-			other._parent._children.remove(other)
-		self._children.add(other)
-		other.parent = self
+        return [self]
 
-	def dump(self, print=print, level=0):
-		padding = '\t' * level
-		if self.is_placeholder:
-			print('%s[Placeholder %s]' % (padding, self.message.id))
-		else:
-			print('%sId: %s' % (padding, self.message.id))
-			print('%sSubject: %s' % (padding, self.message.subject))
+    def add_child(self, other):
+        if self.can_reach(other) or other.can_reach(self):
+            #print('would loop %r => %r' % (self, other))
+            return
+        if other._parent is not None:
+            other._parent._children.remove(other)
+        self._children.add(other)
+        other._parent = self
 
-		for c in self._children:
-			c.dump(print, level+1)
+    def dump(self, print=print, level=0):
+        padding = '\t' * level
+        if self.is_placeholder:
+            print('%s[Placeholder %s]' % (padding, self.message.id))
+        else:
+            print('%sId: %s' % (padding, self.message.id))
+            print('%sSubject: %s' % (padding, self.message.subject))
 
-	def __repr__(self):
-		return '<Container (of %r)>' % self.message.id
+        for c in self._children:
+            c.dump(print, level + 1)
+
+    def __repr__(self):
+        return '<Container (of %r)>' % self.message.id
 
 
 class Table(dict):
-	@property
-	def root_set(self):
-		for v in self.values():
-			if v.is_root:
-				yield v
+    @property
+    def root_set(self):
+        for v in self.values():
+            if v.is_root:
+                yield v
 
-	def placeholder(self, message_id):
-		return Container(message(message_id))
+    def placeholder(self, message_id):
+        return Container(message(message_id))
 
-	def add_message(self, message):
-		try:
-			container = self[message.id]
-			if container.is_placeholder:
-				container.message = message
-		except KeyError as e:
-			container = Container(message)
-			self[message.id] = container
+    def add_message(self, message):
+        try:
+            container = self[message.id]
+            if container.is_placeholder:
+                container.message = message
+        except KeyError as e:
+            container = Container(message)
+            self[message.id] = container
 
-		lastc = None
-		for ref in message.ref:
-			rcontainer = self.setdefault(ref, self.placeholder(ref))
-			if lastc is not None:
-				lastc.add_child(rcontainer)
-			lastc = rcontainer
+        lastc = None
+        for ref in message.ref:
+            rcontainer = self.setdefault(ref, self.placeholder(ref))
+            if lastc is not None:
+                lastc.add_child(rcontainer)
+            lastc = rcontainer
 
-		if lastc is not None:
-			lastc.add_child(container)
+        if lastc is not None:
+            lastc.add_child(container)
 
 
 def thread(messages):
-	table = Table()
-	for message in messages:
-		table.add_message(message)
-	for c in table.root_set:
-		nc, = c.prune()
-		## subject merge
-		## sorting
-		yield nc
+    table = Table()
+    for message in messages:
+        table.add_message(message)
+    for c in table.root_set:
+        nc, = c.prune()
+        ## subject merge
+        ## sorting
+        yield nc
